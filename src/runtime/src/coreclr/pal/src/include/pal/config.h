a3e41e61ded0360794edc62a67240777d0581708
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2025-01-06 21:10:19 -0800

    Update config.h

--- src/runtime/src/coreclr/pal/src/include/pal/config.h
+++ src/runtime/src/coreclr/pal/src/include/pal/config.h
@@ -1,5 +1,2 @@
-#pragma once
-// Disable __attribute__ when using Visual Studio
-#ifdef _MSC_VER
-#define __attribute__(a)
-#endif
+// src\runtime\src\coreclr\pal\src\include\pal\config.h  
+#pragma once 
